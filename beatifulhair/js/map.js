114c673478e0cf2ce0a39e2f26ea9f3e984495f6
Iuri Matos <iuriimattos@gmail.com>
2017-07-21 20:29:21 -0300

    update

--- beatifulhair/js/map.js
+++
@@ -1,8 +0,0 @@
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(-8.0631495, -34.87131120000004),
-    zoom: 10
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}
