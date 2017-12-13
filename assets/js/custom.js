8f22a4ec7145787cd0d344b2c3dab6a6c0c3ba11
Iuri Matos <iuriimattos@gmail.com>
2017-12-13 01:06:22 -0300

    added mailer by 000webhost.com

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,59 +1,59 @@
 (function($){
 
-	/* ---------------------------------------------- /*
-	 * Preloader
-	/* ---------------------------------------------- */
+    /* ---------------------------------------------- /*
+     * Preloader
+    /* ---------------------------------------------- */
 
-	$(window).load(function() {
-		$('#status').fadeOut();
-		$('#preloader').delay(300).fadeOut('slow');
-	});
+    $(window).load(function() {
+        $('#status').fadeOut();
+        $('#preloader').delay(300).fadeOut('slow');
+    });
 
-	$(document).ready(function() {
+    $(document).ready(function() {
 
-		/* ---------------------------------------------- /*
-		 * Smooth scroll / Scroll To Top
-		/* ---------------------------------------------- */
+        /* ---------------------------------------------- /*
+         * Smooth scroll / Scroll To Top
+        /* ---------------------------------------------- */
 
-		$('a[href*=#]').bind("click", function(e){
-           
-			var anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top
-			}, 1000);
-			e.preventDefault();
-		});
+        $('a[href*=#]').bind("click", function(e){
 
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > 100) {
-				$('.scroll-up').fadeIn();
-			} else {
-				$('.scroll-up').fadeOut();
-			}
-		});
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top
+            }, 1000);
+            e.preventDefault();
+        });
 
-		/* ---------------------------------------------- /*
-		 * Navbar
-		/* ---------------------------------------------- */
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
 
-		$('.header').sticky({
-			topSpacing: 0
-		});
+        /* ---------------------------------------------- /*
+         * Navbar
+        /* ---------------------------------------------- */
 
-		$('body').scrollspy({
-			target: '.navbar-custom',
-			offset: 70
-		})
+        $('.header').sticky({
+            topSpacing: 0
+        });
 
-        
+        $('body').scrollspy({
+            target: '.navbar-custom',
+            offset: 70
+        })
+
+
         /* ---------------------------------------------- /*
 		 * Skills
-        /* ---------------------------------------------- */    
+        /* ---------------------------------------------- */
         //var color = $('#home').css('backgroundColor');
 
         $('.skills').waypoint(function(){
             $('.chart').each(function(){
-            $(this).easyPieChart({
+                $(this).easyPieChart({
                     size:140,
                     animate: 2000,
                     lineCap:'butt',
@@ -64,113 +64,112 @@
                 });
             });
         },{offset:'80%'});
-        
-        
+
+
         /* ---------------------------------------------- /*
 		 * Quote Rotator
 		/* ---------------------------------------------- */
-       
-			$( function() {
-				/*
-				- how to call the plugin:
-				$( selector ).cbpQTRotator( [options] );
-				- options:
-				{
-					// default transition speed (ms)
-					speed : 700,
-					// default transition easing
-					easing : 'ease',
-					// rotator interval (ms)
-					interval : 8000
-				}
-				- destroy:
-				$( selector ).cbpQTRotator( 'destroy' );
-				*/
 
-				$( '#cbp-qtrotator' ).cbpQTRotator();
+        $( function() {
+            /*
+            - how to call the plugin:
+            $( selector ).cbpQTRotator( [options] );
+            - options:
+            {
+                // default transition speed (ms)
+                speed : 700,
+                // default transition easing
+                easing : 'ease',
+                // rotator interval (ms)
+                interval : 8000
+            }
+            - destroy:
+            $( selector ).cbpQTRotator( 'destroy' );
+            */
 
-			} );
-		
-        
-		/* ---------------------------------------------- /*
-		 * Home BG
-		/* ---------------------------------------------- */
+            $( '#cbp-qtrotator' ).cbpQTRotator();
 
-		$(".screen-height").height($(window).height());
-
-		$(window).resize(function(){
-			$(".screen-height").height($(window).height());
-		});
-
-		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-			$('#home').css({'background-attachment': 'scroll'});
-		} else {
-			$('#home').parallax('50%', 0.1);
-		}
+        } );
 
 
-		/* ---------------------------------------------- /*
-		 * WOW Animation When You Scroll
-		/* ---------------------------------------------- */
+        /* ---------------------------------------------- /*
+         * Home BG
+        /* ---------------------------------------------- */
 
-		wow = new WOW({
-			mobile: false
-		});
-		wow.init();
+        $(".screen-height").height($(window).height());
+
+        $(window).resize(function(){
+            $(".screen-height").height($(window).height());
+        });
+
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+            $('#home').css({'background-attachment': 'scroll'});
+        } else {
+            $('#home').parallax('50%', 0.1);
+        }
 
 
-		/* ---------------------------------------------- /*
-		 * E-mail validation
-		/* ---------------------------------------------- */
+        /* ---------------------------------------------- /*
+         * WOW Animation When You Scroll
+        /* ---------------------------------------------- */
 
-		function isValidEmailAddress(emailAddress) {
-			var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-			return pattern.test(emailAddress);
-		};
+        wow = new WOW({
+            mobile: false
+        });
+        wow.init();
 
-		/* ---------------------------------------------- /*
-		 * Contact form ajax
-		/* ---------------------------------------------- */
 
-		$('#contact-form').submit(function(e) {
+        /* ---------------------------------------------- /*
+         * E-mail validation
+        /* ---------------------------------------------- */
 
-			e.preventDefault();
+        function isValidEmailAddress(emailAddress) {
+            var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+            return pattern.test(emailAddress);
+        };
 
-			var c_name = $('#c_name').val();
-			var c_email = $('#c_email').val();
-			var c_message = $('#c_message ').val();
-			var response = $('#contact-form .ajax-response');
+        /* ---------------------------------------------- /*
+         * Contact form ajax
+        /* ---------------------------------------------- */
 
-			if (( c_name== '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
-				response.fadeIn(500);
-				response.html('<i class="fa fa-warning"></i> nomae, email, messagem inválidos ou vazios');
-			}
+        $('#contact-form').submit(function(e) {
 
-			else {
-// jQuery.ajax({
-// 	var c_name = $('#c_name').val();
-// 	var c_email = $('#c_email').val();
-// 	var c_message = $('#c_message ').val();
-// 	url: "../php/email-contato.php",
-//   type: "POST",
-//   data: {
-//       nome: c_name,
-//       tel: c_email,
-//       messagem: c_message
-//   },
-//   success:function(msg){
-// 	response.html(msg).fadeIn(500);
-//   }
-//   error:function(msg){
-//   	response.html(msg).fadeIn(500);
-//   }
-// });
-			
-				}
-            
-            	return false;
-			});
+            e.preventDefault();
 
-	});
+            var c_name = $('#c_name').val();
+            var c_email = $('#c_email').val();
+            var c_phone = $('#c_phone').val();
+            var c_message = $('#c_message ').val();
+            var response = $('#contact-form .ajax-response');
+
+            if (( c_name == '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
+                response.fadeIn(500);
+                response.html('<i class="fa fa-warning"></i> nome, email, telefone ou mensagem inválidos ou vazios');
+            }
+
+            else {
+                jQuery.ajax({
+                    url: "https://iurimatos.000webhostapp.com/mailer.php",
+                    type: "POST",
+                    data: {
+                        name: c_name,
+                        email: c_email,
+                        phone: c_phone,
+                        message: c_message
+                    },
+                    success:function(msg){
+                        response.html(msg).fadeIn(500);
+                    },
+                    error:function(msg){
+                        response.html(msg).fadeIn(500);
+                    }
+                });
+
+            }
+
+            return response;
+        });
+
+    });
 
 })(jQuery);
