b9b19672f81662033aa4b2de82beb8258599c08b
iurimattos <iuriimattos@gmail.com>
2018-11-10 17:57:32 -0200

    force reoad page with https

--- cartas-do-altissimo/js/main.js
+++ cartas-do-altissimo/js/main.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
 
     // https://developers.facebook.com/apps/380087835831757/review-status/
     // Acessar o endereço web https://iurimatos.com.br/cartas-do-altissimo clicar em Administrador (mobile: botao menu depois Administrador) preencher o confirm com vazio (string vazia) logar no facebook, Preencher o campo "Mensagem" e clicar em Enviar
@@ -8,34 +8,44 @@
     var message;
 
     function initFacebookGraph() {
-        window.fbAsyncInit = function() {
+        window.fbAsyncInit = function () {
             FB.init({
-                appId      : '380087835831757',
-                xfbml      : true,
-                version    : 'v3.0'
+                appId: '380087835831757',
+                xfbml: true,
+                version: 'v3.0'
             });
             FB.AppEvents.logPageView();
         };
 
-        (function(d, s, id){
+        (function (d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s);
+            js.id = id;
             js.src = "https://connect.facebook.net/en_US/sdk.js";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
     }
 
+    document.onreadystatechange = function () {
+        if (document.readyState == "interactive") {
+            if (location.protocol != 'https:') {
+                location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+            }
+        }
+    }
+
     function promptAuth() {
-        $('#welcomeADM').click(function() {
+        $('#welcomeADM').click(function () {
             var password = prompt('Insira sua senha administrador(a) :');
-            if(password === '') {
+            if (password === '') {
                 alert('Bem vindo(a)');
                 $('#myModal').modal('show');
                 facebookAuth();
 
-            }
-            else {
+            } else {
                 alert('Senha inválida');
                 return;
             }
@@ -44,10 +54,8 @@
     }
 
     function facebookAuth() {
-        FB.login(function(response)
-        {
-            if (response.authResponse)
-            {
+        FB.login(function (response) {
+            if (response.authResponse) {
                 access_token = response.authResponse.accessToken;
                 FB.api(
                     "/debug_token?input_token=" + access_token,
@@ -55,7 +63,7 @@
                         if (response && !response.error) {
                             if (response.data.is_valid) {
 
-                                FB.api(response.data.user_id + '/accounts',function (response){
+                                FB.api(response.data.user_id + '/accounts', function (response) {
                                     if (response) {
 
                                         response.data.forEach(function (page) {
@@ -82,15 +90,17 @@
 
     function postar() {
 
-        $('#postMessage').click(function() {
+        $('#postMessage').click(function () {
             message = $('#c_message').val();
 
 
             FB.api(
-                '/'+pageId+'/feed',
-                'POST',
-                {"message":message,"access_token":access_token},
-                function(response) {
+                '/' + pageId + '/feed',
+                'POST', {
+                    "message": message,
+                    "access_token": access_token
+                },
+                function (response) {
 
                 }
             );
